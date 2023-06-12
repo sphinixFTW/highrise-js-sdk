a973ef26c7dc958c0aafa3aefd8456e547e8f7a8
Hussein Haidar <sphinixftw@gmail.com>
2023-06-12 13:57:59 -0300

    Removed Package Version Checker

--- src/models/events/handleReadyEvent.js
+++ src/models/events/handleReadyEvent.js
@@ -1,18 +1,11 @@
 const { SessionMetadata } = require("../models");
 const packageJson = require('../../../package.json');
-const { execSync } = require('child_process');
 
 function handleReadyEvent(data, emit) {
   try {
+
     // Emit the 'ready' event with sessionMetadata
-
     const currentVersion = packageJson.version;
-    const latestVersion = execSync('npm view highrise-js-sdk version').toString().trim();
-
-    if (currentVersion !== latestVersion) {
-      console.warn(`[WARNING]:`.red + ` You are using version ` + `${currentVersion}`.yellow + ` of highrise-js-sdk. The latest version is ` + `${latestVersion}`.green + ` Consider updating by running ` + `npm install highrise-js-sdk@latest`.green);
-    }
-
     const sessionMetadata = new SessionMetadata(
       data.user_id,
       data.room_info,
