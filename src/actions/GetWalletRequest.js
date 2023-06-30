ff1d41da71c2152127f3a9f8ef54fa0c87ae7823
Hussein Haidar <sphinixftw@gmail.com>
2023-06-30 14:47:10 -0300

    New wallet methods

--- src/actions/GetWalletRequest.js
+++ src/actions/GetWalletRequest.js
@@ -1,5 +1,5 @@
-const { HighriseApiError } = require("../handlers/errors");
-const { GetWalletRequest, SendPayloadAndGetResponse } = require("../models/models");
+const { HighriseApiError, InvalidCurrencyAmount, InvalidPaymentMethod } = require("../handlers/errors");
+const { GetWalletRequest, SendPayloadAndGetResponse, BuyVoiceTimeRequest, BuyRoomBoostRequest } = require("../models/models");
 
 class Wallet {
   constructor(bot) {
@@ -7,9 +7,72 @@ class Wallet {
     this.reqId = Math.floor(Math.random() * 1000);
   }
 
+  buy = {
+    voice: async (payment_method = 'bot_wallet_priority') => {
+      try {
+        const validPaymentMethods = ["bot_wallet_only", "bot_wallet_priority", "user_wallet_only"];
+        if (!validPaymentMethods.includes(payment_method)) {
+          throw new InvalidPaymentMethod(`Invalid payment method:`.red + "\n" + `${validPaymentMethods.join("\n")}`.green);
+        }
+
+        if (this.bot.ws.readyState === this.bot.websocket.OPEN) {
+          const buyVoiceTimeRequest = new BuyVoiceTimeRequest(payment_method, this.reqId.toString())
+          const payload = {
+            _type: "BuyVoiceTimeRequest",
+            rid: buyVoiceTimeRequest.rid,
+            payment_method: buyVoiceTimeRequest.payment_method
+          }
+
+          const sender = new SendPayloadAndGetResponse(this.bot); // Create an instance of SendPayloadAndGetResponse
+          const response = await sender.sendPayloadAndGetResponse(
+            payload,
+            BuyVoiceTimeRequest.Response
+          );
+
+          return response.result.result;
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+
+    boost: async (payment_method = 'bot_wallet_priority', amount = 1) => {
+      try {
+        const validPaymentMethods = ["bot_wallet_only", "bot_wallet_priority", "user_wallet_only"];
+        if (!validPaymentMethods.includes(payment_method)) {
+          throw new InvalidPaymentMethod(`Invalid payment method:`.red + "\n" + `${validPaymentMethods.join("\n")}`.green);
+        }
+
+        if (!Number.isInteger(amount)) {
+          throw new InvalidCurrencyAmount("Invalid currency amount.".red);
+        }
+
+        if (this.bot.ws.readyState === this.bot.websocket.OPEN) {
+          const buyRoomBoostRequest = new BuyRoomBoostRequest(payment_method, amount, this.reqId.toString());
+          const payload = {
+            _type: "BuyRoomBoostRequest",
+            rid: buyRoomBoostRequest.rid,
+            payment_method: buyRoomBoostRequest.payment_method,
+            amount: buyRoomBoostRequest.amount
+          };
+
+          const sender = new SendPayloadAndGetResponse(this.bot); // Create an instance of SendPayloadAndGetResponse
+          const response = await sender.sendPayloadAndGetResponse(
+            payload,
+            BuyRoomBoostRequest.Response
+          );
+
+          return response.result.result;
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+  }
+
   async fetch() {
     try {
-
       if (this.bot.ws.readyState === this.bot.websocket.OPEN) {
         const getWalletRequest = new GetWalletRequest(this.reqId.toString());
         const payload = {
@@ -17,16 +80,15 @@ class Wallet {
           rid: getWalletRequest.rid
         };
 
-        const sender = new SendPayloadAndGetResponse(this.bot); // Create an instance of SendPayloadAndGetResponse
+        const sender = new SendPayloadAndGetResponse(this.bot);
         const response = await sender.sendPayloadAndGetResponse(
           payload,
           GetWalletRequest.Response
         );
 
-        this.walletData = response.content.content[0]; // Store the wallet data
+        this.walletData = response.content.content; // Store the wallet data
 
         return this.walletData;
-
       }
     } catch (error) {
       const highriseError = new HighriseApiError("Error fetching wallet request:", error);
@@ -35,32 +97,68 @@ class Wallet {
   }
 
   get = {
-    amount: async () => {
-      try {
-        if (!this.walletData) {
-          await this.fetch();
+    gold: {
+      amount: async () => {
+        try {
+          if (!this.walletData) {
+            await this.fetch();
+          }
+          const goldAmount = this.walletData.find(item => item.type === 'gold')?.amount || 0;
+          return goldAmount;
+        } catch (error) {
+          console.error(error);
         }
-        const amount = this.walletData.amount;
-        return amount;
-
-      } catch (error) {
-        console.error(error);
       }
     },
-
-    type: async () => {
-      try {
-        if (!this.walletData) {
-          await this.fetch();
+    boost: {
+      amount: async () => {
+        try {
+          if (!this.walletData) {
+            await this.fetch();
+          }
+          const boostAmount = this.walletData.find(item => item.type === 'room_boost_tokens')?.amount || 0;
+          return boostAmount;
+        } catch (error) {
+          console.error(error);
         }
-        const type = this.walletData.type;
-        return type;
-
-      } catch (error) {
-        console.error(error);
+      },
+      type: async () => {
+        try {
+          if (!this.walletData) {
+            await this.fetch();
+          }
+          const boostType = this.walletData.find(item => item.type === 'room_boost_tokens')?.type || '';
+          return boostType;
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    },
+    voice: {
+      amount: async () => {
+        try {
+          if (!this.walletData) {
+            await this.fetch();
+          }
+          const voiceAmount = this.walletData.find(item => item.type === 'room_voice_tokens')?.amount || 0;
+          return voiceAmount;
+        } catch (error) {
+          console.error(error);
+        }
+      },
+      type: async () => {
+        try {
+          if (!this.walletData) {
+            await this.fetch();
+          }
+          const voiceType = this.walletData.find(item => item.type === 'room_voice_tokens')?.type || '';
+          return voiceType;
+        } catch (error) {
+          console.error(error);
+        }
       }
     }
-  }
+  };
 
 }
 
